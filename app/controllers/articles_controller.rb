535967f0e393124cd78a42f2ba485fb277a1166a
出口友梨 <yurinco1119@gmail.com>
2022-09-12 21:31:33 +0900

    記事の削除機能追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,11 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy!
+    redirect_to root_path, notice: '削除に成功しました'
+  end
 
   private
   def article_params

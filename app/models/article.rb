af090c195cbdc6b2e93937e08dfe365ee4b50ab3
出口友梨 <yurinco1119@gmail.com>
2022-09-20 14:33:46 +0900

    手直し

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
-  validates :title, format: { with: /\A(?!\@)/ }
+  validates :title, format: { with: /\A(?!@)/ }
 
   validates :content, presence: true
   validates :content, length: { minimum: 10 }
@@ -20,12 +20,13 @@ class Article < ApplicationRecord
   validate :validate_title_and_content_length
 
   def display_created_at
-    I18n.l(self.created_at, format: :default)
+    I18n.l(created_at, format: :default)
   end
 
   private
+
   def validate_title_and_content_length
-    char_count = self.title.length + self.content.length
+    char_count = title.length + content.length
     errors.add(:content, '100文字いじょうで！') unless char_count > 100
   end
 end

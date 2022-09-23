db4c5fefb2cb724fe84705c5f21f7e1e28b68d91
出口友梨 <yurinco1119@gmail.com>
2022-09-23 17:55:56 +0900

    プロフィールの編集、更新、年齢を表示

---
+++ .history/app/controllers/profiles_controller_20220923165508.rb
@@ -0,0 +1,11 @@
+class ProfilesController < ApplicationController
+before_action :authenticate_user!
+
+    def show
+        @profile = current_user.profile
+    end
+
+    def edit
+        @profile = current_user.build_profile
+    end
+end
---
+++ .history/app/controllers/profiles_controller_20220923171925.rb
@@ -0,0 +1,16 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @profile = current_user.profile
+  end
+
+  def edit
+    @profile = current_user.build_profile
+  end
+
+  def updated
+  end
+
+  private
+end
